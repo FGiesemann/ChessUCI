0bcfd0bfbe622cd7f85fd8b28e5ee219d2d69dd7
Florian Giesemann <florian.giesemann@gmail.com>
2025-10-26 13:17:21 +0100

    Define UCI data structs

---
+++ src/protocol.cpp
@@ -0,0 +1,8 @@
+/* ************************************************************************** *
+ * Chess UCI                                                                  *
+ * Universal Chess Interface for Chess Engines                                *
+ * ************************************************************************** */
+
+#include "chessuci/protocol.h"
+
+namespace chessuci {}
