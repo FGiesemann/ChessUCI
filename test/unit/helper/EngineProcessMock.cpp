8da423ff8d574101d6d957fcb376f136eb8edb6a
Florian Giesemann <florian.giesemann@gmail.com>
2025-11-03 18:06:49 +0100

    Fix locking and terminate/kill command

--- test/unit/helper/EngineProcessMock.cpp
+++ test/unit/helper/EngineProcessMock.cpp
@@ -7,8 +7,6 @@
 
 #include <thread>
 
-#include <iostream>
-
 namespace chessuci::test {
 
 auto EngineProcessMock::when_receives(const std::string &input, ResponseFunction response) -> void {
@@ -16,35 +14,35 @@ auto EngineProcessMock::when_receives(const std::string &input, ResponseFunction
 }
 
 auto EngineProcessMock::write_line(const std::string &line) -> bool {
-    std::cout << "Mock::write_line(): " << line << std::endl;
     auto it = m_responses.find(line);
     if (it != m_responses.end()) {
-        std::cout << "Mock::write_line(): Found response for " << line << std::endl;
         const auto responses = it->second(line);
         std::lock_guard<std::mutex> lock{m_queue_mutex};
         for (const auto &response : responses) {
-            std::cout << "Mock::write_line(): Adding response " << response << std::endl;
             m_pending_responses.push(response);
         }
-    } else {
-        std::cout << "Mock::write_line(): No response for " << line << std::endl;
     }
     return true;
 }
 
+auto EngineProcessMock::can_read() const -> bool {
+    std::lock_guard<std::mutex> lock{m_queue_mutex};
+    return !m_pending_responses.empty();
+}
+
 auto EngineProcessMock::read_line(std::string &line) -> bool {
-    std::cout << "Mock::read_line()" << std::endl;
-    while (true) {
-        std::lock_guard<std::mutex> lock{m_queue_mutex};
-        if (!m_pending_responses.empty()) {
+    while (m_running) {
+        if (can_read()) {
             break;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
+    if (!m_running) {
+        return false;
+    }
     std::lock_guard<std::mutex> lock{m_queue_mutex};
     line = std::move(m_pending_responses.front());
     m_pending_responses.pop();
-    std::cout << "Mock::read_line(): " << line << std::endl;
 
     return true;
 }
